bd69e925787c751fbb3e30ba6afe8d71493c4d96
Keshav Rajput <keshav.rajput@walmart.com>
2021-04-16 00:27:48 -0500

    updating allocation by name

--- src/components/pages/Allocations.js
+++ src/components/pages/Allocations.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+
 
 
 class Allocations extends Component{
@@ -28,12 +31,27 @@ class Allocations extends Component{
           
           fetch( "https://cric-fap.herokuapp.com/v1/iplt20/allocations", requestOptions)
             .then(response => response.text())
-            .then(result => {result = JSON.parse(result); console.log(typeof result); this.setState({
+            .then(result => {result = JSON.parse(result); console.log(typeof result); 
+                this.setState({
                 allocations: result
             })})
             .catch(error => console.log('error', error));
     }
 
+    getAllocationsByName = (name) => {
+        console.log(name+"NAMEEE")
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+          fetch( "https://cric-fap.herokuapp.com/v1/iplt20/allocationsByName?userId="+name, requestOptions)
+            .then(response => response.text())
+            .then(result => {result = JSON.parse(result); console.log(typeof result); 
+                this.setState({
+                allocations: result
+            })})
+            .catch(error => console.log('error', error));
+    }
 
     render(){
         const {
@@ -45,6 +63,22 @@ class Allocations extends Component{
                 <Button variant="success" size="lg" block>
                     Player Allocations
                 </Button>
+                <DropdownButton id="dropdown-basic-button" title="Kiske Dekhne hain ?">
+                    <Dropdown.Item onSelect={() => this.getAllocations()}> All</Dropdown.Item>
+                    <Dropdown.Item onSelect={() => this.getAllocationsByName("Akash")}> Akash</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Devang")}>Devang</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Keyur")}>Keyur</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Keshav")}>Keshav</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Parth")}>Parth</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Deepu")}>Deepu</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Lakhan")}>Lakhan</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Sid")}>Sid</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Sachin")}>Sachin</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Swapnil")}>Swapnil</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Ved")}>Ved</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Dhruvik")}>Dhruvik</Dropdown.Item>
+                    <Dropdown.Item onSelect={() =>this.getAllocationsByName("Tanmay")}>Tanmay</Dropdown.Item>
+                </DropdownButton>
                <Table striped bordered hover size="sm" responsive>
                 <thead>
                     <tr>
@@ -65,8 +99,6 @@ class Allocations extends Component{
                 })}
                 </tbody>
                 </Table>
-            
-        
         </div>
 
     
